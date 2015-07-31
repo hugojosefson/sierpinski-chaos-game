fa48cf969358a08a679b78ffdef3da3f9c411c4f
Hugo Josefson <hugo@josefson.org>
2015-07-31 22:47:42 +0200

    Canvas.

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 'use strict';
 
+var fit = require('canvas-fit');
+var canvas = document.createElement('canvas');
+document.body.appendChild(canvas);
+
+var resize = fit(canvas);
+resize();
+window.addEventListener('resize', resize, false);
