cf47019d01022007adb3523b2245e558a42b5712
Hugo Josefson <hugo@josefson.org>
2015-08-01 00:34:41 +0200

    Refactor.

--- index.js
+++ index.js
@@ -1,25 +1,24 @@
 'use strict';
 
-var fit = require('canvas-fit');
-var canvas = document.createElement('canvas');
-document.body.appendChild(canvas);
+var corners;
 
-var resize = fit(canvas);
-resize();
-window.addEventListener('resize', resize, false);
-
-var ctx = canvas.getContext('2d');
+function init({width, height}) {
+    corners = [
+        [width * 1 / 3, 10],
+        [10, height - 10],
+        [width - 10, height * 2 / 3]
+    ];
+}
 
 var times = require('lodash.times');
 var setPixel = require('./lib/set-pixel');
 var next = require('./lib/next')('seed');
-var corners = [
-    [canvas.width * 1 / 3, 10],
-    [10, canvas.height - 10],
-    [canvas.width - 10, canvas.height * 2 / 3]
-];
-var p = [100, 100];
-times(10000, () => {
-    p = next(p, corners);
-    setPixel(ctx, p);
-});
+function draw({ctx}) {
+    var p = [100, 100];
+    times(10000, () => {
+        p = next(p, corners);
+        setPixel(ctx, p);
+    });
+}
+
+require('./lib/canvas')(init, draw);
